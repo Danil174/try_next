c79a9b50958898150ea511000c3fdb8400cd3f8c
danil milostiviy <milostiviy.danil@yandex.ru>
2020-09-14 11:53:11 +0300

    progressbar

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,21 @@
 import Router from "next/router";
 import {MainLayout} from "../../components/MainLayout";
 
-export default function About() {
+export default function About({title}) {
   return (
     <MainLayout title={'About page'}>
-      <h2>About page</h2>
+      <h2>{title}</h2>
       <button onClick={() => Router.push("/")}>Go back to home</button>
       <button onClick={() => Router.push("/posts")}>Go to posts</button>
     </MainLayout>
   );
+}
+
+About.getInitialProps = async () => {
+  const response = await fetch('http://localhost:4200/about');
+  const data = await response.json();
+
+  return {
+    title: data.title
+  }
 }
